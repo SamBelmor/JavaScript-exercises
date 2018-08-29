989ff03798b55d46b2506d41a58191d40066c62c
Dzoara Bello <sambelmor@gmail.com>
2018-08-29 13:51:03 -0500

    fix if a number is prime or not

--- functions/isPrime.js
+++ functions/isPrime.js
@@ -1,15 +1,12 @@
-let isPrime = number => {
-  for (let f = 2; f < number; f++) {
-    if (number % f === 0) {
-      return `${number} isn't a prime number`;
+isPrime = num => {
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0) {
+      return 'Is not a prime number';
     }
-    else{
-      return `${number} is a primer number!`;
-    } 
   }
-  if (number === 1) {
-    return `${number} isn't a prime number`;
+  if (num !== 1) {
+    return 'Is a prime number!!';
   }
 }
 
-console.log(isPrime(17));
+console.log(isPrime(2));
